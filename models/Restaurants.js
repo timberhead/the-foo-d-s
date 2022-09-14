283bcbf1b48f0fc319a40a2be8fd9d5bfbd1cdfa
Dragon Lore <tate.j.foster@gmail.com>
2022-09-14 14:41:36 -0600

    set up table formatting for Dishes, Foodtypes, Restaurants, locations and relationships

---
+++ models/Restaurants.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Restaurants extends Model {}
+
+Restaurants.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: { type: DataTypes.STRING, allowNull: false },
+		type_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "food_types",
+				key: "id",
+			},
+		},
+	},
+	{ sequelize, underscored: true, modelName: "restaurants" }
+);
