d922455654041186e261c47c759b595f5c3ea245
Charles Hargrave <charles.hargrave.jr@gmail.com>
2022-09-15 14:42:51 -0600

    merge

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = process.env.JAWSDB_URL
-<<<<<<< HEAD
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
       process.env.DB_NAME,
@@ -14,18 +13,5 @@ const sequelize = process.env.JAWSDB_URL
         port: 3306,
       }
     );
-=======
-	? new Sequelize(process.env.JAWSDB_URL)
-	: new Sequelize(
-			process.env.DB_NAME,
-			process.env.DB_USER,
-			process.env.DB_PASSWORD,
-			{
-				host: "localhost",
-				dialect: "mysql",
-				port: 3306,
-			}
-	  );
->>>>>>> origin
 
 module.exports = sequelize;
