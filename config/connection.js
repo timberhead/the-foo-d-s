ecca70bb96b7828c630e8e8779779a248d255c07
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-09-15 12:54:28 -0600

    adding styling to the homepage and adjusting css files.

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,17 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-<<<<<<< HEAD
 const sequelize = process.env.JAWSDB_URL
-	? new Sequelize(process.env.JAWSDB_URL)
-	: new Sequelize(
-			process.env.DB_NAME,
-			process.env.DB_USER,
-			process.env.DB_PASSWORD,
-			{
-				host: "localhost",
-				dialect: "mysql",
-				port: 3306,
-			}
-	  );
-=======
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
->>>>>>> main
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
 
 module.exports = sequelize;
