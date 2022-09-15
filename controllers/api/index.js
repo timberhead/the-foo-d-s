e8c1802ee7053fd59a4cc7eb7e39292ce0d9ef3f
Dragon Lore <tate.j.foster@gmail.com>
2022-09-15 13:21:26 -0600

    testing changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const reviewRoutes = require("./review-routes");
 const restaurantRoutes = require("./restaurant-routes");
 
 router.use("/user", userRoutes);
-router.use("/review", reviewRoutes);
-router.use("/restaurant", restaurantRoutes);
+// router.use("/review", reviewRoutes);
+// router.use("/restaurant", restaurantRoutes);
 
 module.exports = router;
--- seeds/locationData.js
+++ seeds/locationData.js
@@ -1,28 +1,30 @@
 const { Location } = require("../models");
 
 const locationData = [
-  {
-    street_address: "1644 Platte St",
-    city: "Denver",
-    state: "CO",
-    zipcode: 80202,
-    restaurant_id: 1,
-  },
-  {
-    street_address: "1817 Central St",
-    city: "Denver",
-    state: "CO",
-    zipcode: 80211,
-    restaurant_id: 2,
-  },
-  {
-    street_address: "3211 N Pecos St",
-    city: "Denver",
-    state: "CO",
-    zipcode: 80211,
-    restaurant_id: 3,
-  },
+	{
+		street_address: "1644 Platte St",
+		city: "Denver",
+		state: "CO",
+		zipcode: 80202,
+		restaurant_id: 1,
+	},
+	{
+		street_address: "1817 Central St",
+		city: "Denver",
+		state: "CO",
+		zipcode: 80211,
+		restaurant_id: 2,
+	},
+	{
+		street_address: "3211 N Pecos St",
+		city: "Denver",
+		state: "CO",
+		zipcode: 80211,
+		restaurant_id: 3,
+	},
 ];
+
+console.log(locationData);
 
 const seedLocation = () => Location.bulkCreate(locationData);
 
