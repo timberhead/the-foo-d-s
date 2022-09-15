3c1013192069a2caf1b2704cbf428ab00c6cf9ad
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-09-15 11:16:44 -0600

    connected tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  // make sure views is the right path
+  content: ["./views/**/*.{html,js} */"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
