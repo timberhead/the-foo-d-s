20bec6ed0058393bc738ea7a382bf54e2addc143
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-09-15 11:13:24 -0600

    adjusted a comment

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-	secret: "Super secret secret",
-	cookie: {},
-	resave: false,
-	saveUninitialized: true,
-	store: new SequelizeStore({
-		db: sequelize,
-	}),
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening"));
 });
