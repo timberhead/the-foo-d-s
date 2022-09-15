2ac4e3df5b4c87b1f5f6bda64e7a9e7de6b58e25
SLeBron <sophieclebron@gmail.com>
2022-09-15 13:17:40 -0600

    updated price and restaurant id on seeds

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
-const reviewRoutes = require("./review-routes");
-const restaurantRoutes = require("./restaurant-routes");
+// const reviewRoutes = require("./review-routes");
+// const restaurantRoutes = require("./restaurant-routes");
 
 router.use("/user", userRoutes);
 router.use("/review", reviewRoutes);
