4d8694d0c182d31e324d7435d79b1c0817166e01
Dragon Lore <tate.j.foster@gmail.com>
2022-09-16 11:14:22 -0600

    updated file structure

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,63 @@
 const router = require("express").Router();
-const { FoodTypes, Restaurants } = require("../models");
+const sequelize = require("../config/connection");
+const { FoodTypes, Restaurants, Dishes, DishReviews } = require("../models");
 
 router.get("/", async (req, res) => {
 	try {
-		const typeData = await FoodTypes.findAll({
-			include: [Restaurants],
-		});
+		const typeData = await FoodTypes.findAll({});
 
 		const types = typeData.map((type) => type.get({ plain: true }));
 
-		// res.status(200).json(types);
-		res.render("homepage", { types });
+		res.status(200).json(types);
+		// res.render("homepage", { types });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get("/category/:id", async (req, res) => {
+	try {
+		const typeData = await FoodTypes.findByPk(req.params.id, {
+			include: [{ all: true, nested: true }],
+			attributes: {
+				include: [
+					[
+						sequelize.literal(
+							`SELECT AVG(dish_reviews.rating) FROM dish_reviews JOIN dishes ON dish_reviews.dish_id = dishes.id JOIN restaurants ON dishes.restaurant_id = restaurants.id JOIN food_types ON restaurants.type_id = food_types.id WHERE dishes.restaurant_id = restaurants.id AND food_types.id = ${req.params.id}`
+						),
+						"stars",
+					],
+				],
+			},
+		});
+
+		const type = typeData.get({ plain: true });
+
+		res.status(200).json(type);
+		// res.render("category", { type });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get("/restaurant/:id", async (req, res) => {
+	try {
+		const restaurantData = await Restaurants.findByPk(req.params.id, {
+			include: [{ model: Dishes }],
+		});
+
+		const restaurant = restaurantData.get({ plain: true });
+		res.status(200).json(restaurant);
 	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 
 router.get("/login", (req, res) => {
-	// if (req.session.loggedIn) {
-	// 	res.redirect("/");
-	// 	return;
-	// }
+	if (req.session.loggedIn) {
+		res.redirect("/");
+		return;
+	}
 	res.render("login");
 });
 
--- seeds/dishReviewsData.js
+++ seeds/dishReviewsData.js
@@ -2,16 +2,19 @@ const { DishReviews } = require("../models");
 
 const dishReviewsData = [
 	{
+		rating: 5,
 		review: "This is the bomb!",
 		dish_id: 1,
 		user_id: null,
 	},
 	{
+		rating: 1,
 		review: "Best meatball sandwich in the world!!!",
 		dish_id: 2,
 		user_id: null,
 	},
 	{
+		rating: 2,
 		review: "Amazing!",
 		dish_id: 3,
 		user_id: null,
