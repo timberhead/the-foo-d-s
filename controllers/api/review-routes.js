3cfeb445379b15dd0600798f7118466ff6a4cdb5
Dragon Lore <tate.j.foster@gmail.com>
2022-09-16 16:16:05 -0600

    did post put and delete routing for reviews

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -10,13 +10,28 @@ router.post("/", withAuth, async (req, res) => {
 			dish_id: req.body.dish_id,
 			user_id: req.session.userId,
 		});
+
+		// res.status(200).json(newReview);
+		res.redirect(`/dish/${req.body.dish_id}`);
 	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 
-router.put("/", withAuth, async (req, res) => {
+router.put("/:id", withAuth, async (req, res) => {
 	try {
+		const [affectedRows] = await DishReviews.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		if (affectedRows > 0) {
+			res.status(200).end();
+			// res.redirect(`/dish/${req.body.dish_id}`);
+		} else {
+			res.status(404).end();
+		}
 	} catch (err) {
 		res.status(500).json(err);
 	}
@@ -24,6 +39,18 @@ router.put("/", withAuth, async (req, res) => {
 
 router.delete("/:id", withAuth, (req, res) => {
 	try {
+		const [affectedRows] = DishReviews.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		if (affectedRows > 0) {
+			res.status(200).end();
+			// res.redirect(`/dish/${req.body.dish_id}`);
+		} else {
+			res.status(404).end();
+		}
 	} catch (err) {
 		res.status(500).json(err);
 	}
