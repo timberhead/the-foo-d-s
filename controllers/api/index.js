05526be569750d29646f3ca53001769bfb63c636
SLeBron <sophieclebron@gmail.com>
2022-09-16 14:47:42 -0600

    updating

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
 const reviewRoutes = require("./review-routes");
-const restaurantRoutes = require("./restaurant-routes");
+// const restaurantRoutes = require("./restaurant-routes");
 
 router.use("/user", userRoutes);
 router.use("/review", reviewRoutes);
